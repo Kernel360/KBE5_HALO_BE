838c982096d383080095868287577cf4ff2035b4
sujeong <suujeong0@gmail.com>
2025-05-20 15:07:26 +0900

    feat : ApiResponse 추가

---
+++ src/main/java/com/kernel360/Halo/web/common/ApiResponse.java
@@ -0,0 +1,12 @@
+package com.kernel360.Halo.web.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponse <T> {
+     private boolean success;
+     private String message;
+     private T body;
+}
