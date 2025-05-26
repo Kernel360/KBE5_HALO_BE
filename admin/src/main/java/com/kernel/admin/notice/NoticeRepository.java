70b029e5a5761aed78121f57780631c77fc965f3
hyeyoooo <12qwpo50@gmail.com>
2025-05-26 17:37:39 +0900

    refactor: 불필요한 import 삭제

--- admin/src/main/java/com/kernel/admin/notice/NoticeRepository.java
+++ admin/src/main/java/com/kernel/admin/notice/NoticeRepository.java
@@ -3,7 +3,6 @@ package com.kernel.admin.notice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface NoticeRepository extends  JpaRepository<Notice, Long> {
     List<Notice> findByNoticeType(NoticeType noticeType);
