25fc0838f918245c1c1ec90c186d9013836497d9
SighingOwl <whddms1208@gmail.com>
2025-05-27 13:47:17 +0900

    refactor: Modify file pathes of admin module to admin package in common module due to project structure has benn changed.

--- common/src/main/java/com/kernel/common/global/entity/BaseEntity.java
+++ common/src/main/java/com/kernel/common/global/entity/BaseEntity.java
@@ -11,7 +11,6 @@ import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
