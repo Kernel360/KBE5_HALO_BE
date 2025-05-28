cb163824910da04685a15d4b2ac460bcb2aeb7d8
SighingOwl <whddms1208@gmail.com>
2025-05-28 15:51:08 +0900

    feat: Add QueryDSL Configuration.

--- customer/src/main/java/com/kernel/customer/repository/InquiryRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.kernel.customer.repository;
-
-import com.kernel.customer.entity.Inquiry;
-
-import java.util.List;
-
-public interface InquiryRepositoryCustom {
-    List<Inquiry> findByIdCustom(Long keyword);
-}
