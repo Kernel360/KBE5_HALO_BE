f4d78e91f9469d5f7a0e24eb2e0ff5f24480617c
from-ryw <from.ryw@gmail.com>
2025-05-30 17:12:14 +0900

    chore: 누락된 ManagerReviewService 파일 추가

---
+++ common/src/main/java/com/kernel/common/manager/service/ManagerReviewServiceImpl.java
@@ -0,0 +1,44 @@
+package com.kernel.common.manager.service;
+
+import com.kernel.common.global.enums.AuthorType;
+import com.kernel.common.manager.dto.mapper.ManagerReviewMapper;
+import com.kernel.common.manager.dto.request.ManagerReviewReqDTO;
+import com.kernel.common.manager.dto.response.ManagerReviewRspDTO;
+import com.kernel.common.manager.repository.ManagerReviewRepository;
+import com.kernel.common.reservation.entity.Review;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ManagerReviewServiceImpl implements ManagerReviewService {
+
+    private final ManagerReviewRepository managerReviewRepository;
+    private final ManagerReviewMapper managerReviewMapper;
+
+
+    /**
+     * 매니저 리뷰 등록
+     * @param authorId 작성자ID(=매니저ID)
+     * @param reservationId 예약ID
+     * @param managerReviewReqDTO 매니저 리뷰 등록 요청 데이터
+     * @return 작성된 리뷰 정보를 담은 응답
+     */
+    @Override
+    public ManagerReviewRspDTO createManagerReview(Long authorId, Long reservationId, ManagerReviewReqDTO managerReviewReqDTO) {
+
+        // 등록된 리뷰가 존재하는지 확인
+        if (managerReviewRepository.existsByAuthorTypeAndAuthorIdAndReservationId(AuthorType.MANAGER, authorId, reservationId)) {
+            throw new IllegalStateException("이미 등록된 리뷰가 존재합니다.");
+        }
+
+        // RequestDTO -> Entity
+        Review reviewedEntity = managerReviewMapper.toEntity(authorId, reservationId, managerReviewReqDTO);
+
+        // 저장
+        Review savedReview = managerReviewRepository.save(reviewedEntity);
+
+        // Entity -> ResponseDTO 후, return
+        return managerReviewMapper.toResponseDTO(savedReview);
+    }
+}
