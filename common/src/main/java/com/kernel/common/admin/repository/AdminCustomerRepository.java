080051b538e65fd0d19694c7444f25175628759b
hyeyoooo <12qwpo50@gmail.com>
2025-05-30 17:44:06 +0900

    fix: 잘못된 타입 변경

--- common/src/main/java/com/kernel/common/admin/repository/AdminCustomerRepository.java
+++ common/src/main/java/com/kernel/common/admin/repository/AdminCustomerRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AdminCustomerRepository extends JpaRepository<AdminCustomer, Long> {
-    List<AdminCustomer> findByCustomerContaining(String keyword);
+    List<AdminCustomer> findByCustomerId(Long customerId);
 }
