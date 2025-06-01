d286bf9687b68fe47d3005b944888dcc6c4fe9cd
SujeongJeong <suujeong0@gmail.com>
2025-06-01 20:36:01 +0900

    feat : 수요자 마이페이지 구현

--- api/src/main/java/com/kernel/app/dto/mapper/CustomerAuthMapper.java
+++ api/src/main/java/com/kernel/app/dto/mapper/CustomerAuthMapper.java
@@ -1,6 +1,7 @@
 package com.kernel.app.dto.mapper;
 
 import com.kernel.common.customer.dto.request.CustomerSignupReqDTO;
+import com.kernel.common.customer.dto.response.CustomerInfoDetailRspDTO;
 import com.kernel.common.customer.entity.Customer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,7 +13,7 @@ public class CustomerAuthMapper {
 
    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    // RequestDTO -> Entity
+    // CustomerSignupReqDTO -> Customer (회원가입)
     public Customer toEntity(CustomerSignupReqDTO dto){
         return Customer.builder()
                 .email(dto.getEmail())
@@ -25,6 +26,22 @@ public class CustomerAuthMapper {
                 .roadAddress(dto.getRoadAddress())
                 .detailAddress(dto.getDetailAddress())
                 .status(dto.getStatus())
+                .build();
+    }
+
+    // Customer -> CustomerInfoDetailRspDTO (유저 상세 정보)
+    public CustomerInfoDetailRspDTO toInfoDto(Customer customer){
+        return CustomerInfoDetailRspDTO.builder()
+                .phone(customer.getPhone())
+                .email(customer.getEmail())
+                .username(customer.getUserName())
+                .birthDate(customer.getBirthDate())
+                .gender(customer.getGender())
+                .zipcode(customer.getZipcode())
+                .roadAddress(customer.getRoadAddress())
+                .detailAddress(customer.getDetailAddress())
+                .point(customer.getPoint())
+                .status(customer.getStatus())
                 .build();
     }
 
