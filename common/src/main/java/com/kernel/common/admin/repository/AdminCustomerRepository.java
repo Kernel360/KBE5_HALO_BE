966f06479c3771b6a37efd5e2eeda8b7a9ee7c80
hyeyoooo <12qwpo50@gmail.com>
2025-06-02 20:10:42 +0900

    refactor: findByPhone 수정

--- common/src/main/java/com/kernel/common/admin/repository/AdminCustomerRepository.java
+++ common/src/main/java/com/kernel/common/admin/repository/AdminCustomerRepository.java
@@ -1,10 +1,11 @@
 package com.kernel.common.admin.repository;
 
-import com.kernel.common.admin.entity.AdminCustomer;
+import com.kernel.common.customer.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface AdminCustomerRepository extends JpaRepository<AdminCustomer, Long> {
-    List<AdminCustomer> findByUserName(String keyword);
+public interface AdminCustomerRepository extends JpaRepository<Customer, Long> {
+
+    List<Customer> findByPhone(String phone);
 }
