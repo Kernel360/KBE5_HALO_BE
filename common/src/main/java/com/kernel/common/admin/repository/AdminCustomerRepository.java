21cc4f196b9c7a0376dc0811b093ae81af326b13
hyeyoooo <12qwpo50@gmail.com>
2025-06-02 17:09:51 +0900

    refactor: ERD name 수정

--- common/src/main/java/com/kernel/common/admin/repository/AdminCustomerRepository.java
+++ common/src/main/java/com/kernel/common/admin/repository/AdminCustomerRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AdminCustomerRepository extends JpaRepository<AdminCustomer, Long> {
-    List<AdminCustomer> findByCustomerNameContaining(String keyword);
+    List<AdminCustomer> findByUserName(String keyword);
 }
