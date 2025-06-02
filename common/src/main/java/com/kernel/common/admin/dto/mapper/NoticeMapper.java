5a321e560a39595989fc970e65fb0b814a883c7e
hyeyoooo <12qwpo50@gmail.com>
2025-06-02 12:06:07 +0900

    refactor: DTO -> Mapper

---
+++ common/src/main/java/com/kernel/common/admin/dto/mapper/NoticeMapper.java
@@ -0,0 +1,18 @@
+package com.kernel.common.admin.dto.mapper;
+
+import com.kernel.common.admin.dto.response.NoticeResDto;
+import com.kernel.common.admin.entity.Notice;
+
+public class NoticeMapper {
+    public static NoticeResDto toDto(Notice notice) {
+        return NoticeResDto.builder()
+                .noticeId(notice.getNoticeId())
+                .noticeType(notice.getNoticeType())
+                .title(notice.getTitle())
+                .content(notice.getContent())
+                .fileId(notice.getFile_Id())
+                .deleted(notice.getIsDeleted())
+                .views(notice.getViews())
+                .build();
+    }
+}
