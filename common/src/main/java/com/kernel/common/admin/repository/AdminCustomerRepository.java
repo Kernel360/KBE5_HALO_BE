d604033da309a3200c1595f78850705d5899b260
hyeyoooo <12qwpo50@gmail.com>
2025-06-02 12:18:17 +0900

    refactor: customerId -> keyword 변경

--- common/src/main/java/com/kernel/common/admin/repository/AdminCustomerRepository.java
+++ common/src/main/java/com/kernel/common/admin/repository/AdminCustomerRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AdminCustomerRepository extends JpaRepository<AdminCustomer, Long> {
-    List<AdminCustomer> findByCustomerId(Long customerId);
+    List<AdminCustomer> findByCustomerNameContaining(String keyword);
 }
