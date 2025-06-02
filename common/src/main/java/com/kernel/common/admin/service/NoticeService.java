9f787b7ae018d47c19e95d9aeebe890cd47fa1d5
hyeyoooo <12qwpo50@gmail.com>
2025-06-02 12:06:27 +0900

    refactor: ERD 이름과 동일하게 변경

--- common/src/main/java/com/kernel/common/admin/service/NoticeService.java
+++ common/src/main/java/com/kernel/common/admin/service/NoticeService.java
@@ -1,10 +1,15 @@
 package com.kernel.common.admin.service;
 
 import com.kernel.common.admin.dto.request.NoticeReqDto;
+import com.kernel.common.admin.dto.response.NoticeResDto;
 import com.kernel.common.admin.entity.Notice;
+import com.kernel.common.admin.entity.NoticeType;
 import com.kernel.common.admin.repository.NoticeRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class NoticeService {
@@ -23,10 +28,17 @@ public class NoticeService {
                 .content(dto.getContent())
                 // .fileId(null)
                 // TODO:파일 첨부 구현 후 주석 해제
-                .Deleted(false)
+                .isDeleted(false)
                 .views(0L)
                 .build();
 
         return noticeRepository.save(notice);
+    }
+
+    // 공지사항 / 이벤트 목록 조회 메서드 추가
+    public List<NoticeResDto> getNoticeList(NoticeType type) {
+        return noticeRepository.findByNoticeType(type).stream()
+                .map(NoticeResDto::from)
+                .collect(Collectors.toList());
     }
 }
