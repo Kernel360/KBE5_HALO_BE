fc7b1f93dcda888b368e9b6ce17748c4ac2eda0b
sujeong <suujeong0@gmail.com>
2025-06-04 18:47:16 +0900

    rename : 수요자 문의사항 파일명 변경

--- common/src/main/java/com/kernel/common/customer/repository/CustomCustomerInquiryRepository.java
+++ common/src/main/java/com/kernel/common/customer/repository/CustomCustomerInquiryRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
-public interface CustomerInquiryRepositoryCustom {
+public interface CustomCustomerInquiryRepository {
 
     // 수요자 문의사항 조회 및 검색
     Page<CustomerInquiry> searchByAuthorIdAndKeyword(Long customerId, String keyword, Pageable pageable);
