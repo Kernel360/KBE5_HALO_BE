d2e15253db13ec2210c9b0fda6867a5788f780fc
SighingOwl <whddms1208@gmail.com>
2025-06-04 14:14:28 +0900

    fix: FileUploadReqDTO가 MultipartFile을 받지 못하는 문제 수정

--- common/src/main/java/com/kernel/common/global/dto/request/FileUploadReqDTO.java
+++ common/src/main/java/com/kernel/common/global/dto/request/FileUploadReqDTO.java
@@ -2,10 +2,12 @@ package com.kernel.common.global.dto.request;
 
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+@Setter
 @Getter
 public class FileUploadReqDTO {
 
