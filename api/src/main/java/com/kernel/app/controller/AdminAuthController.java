14b1d2a03a3db8826668e96598e894ecd61297b7
SighingOwl <whddms1208@gmail.com>
2025-06-11 11:02:31 +0900

    fix: 관리자 로그인 오류 수정

--- api/src/main/java/com/kernel/app/controller/AdminAuthController.java
+++ api/src/main/java/com/kernel/app/controller/AdminAuthController.java
@@ -2,15 +2,18 @@ package com.kernel.app.controller;
 
 import com.kernel.app.service.AdminAuthService;
 import com.kernel.common.admin.dto.request.AdminSignupReqDTO;
+import com.kernel.common.admin.dto.request.AdminUpdateReqDTO;
+import com.kernel.common.global.AuthenticatedUser;
 import com.kernel.common.global.entity.ApiResponse;
+import com.kernel.common.global.enums.UserStatus;
+import com.kernel.common.manager.dto.request.ManagerUpdateReqDTO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -20,6 +23,11 @@ public class AdminAuthController {
 
     private final AdminAuthService adminAuthService;
 
+    /**
+     * 관리자 회원가입
+     * @param joinDTO 관리자 회원가입 요청 DTO
+     * @return 성공 응답
+     */
     @PostMapping("/signup")
     public ResponseEntity<ApiResponse<Void>> join(@Valid @RequestBody AdminSignupReqDTO joinDTO){
         adminAuthService.join(joinDTO);
@@ -27,5 +35,46 @@ public class AdminAuthController {
         return ResponseEntity.ok(new ApiResponse<>(true, "success", null));
     }
 
+    /**
+     * 관리자 정보 수정 API
+     * @param admin 관리자
+     * @param updateReqDTO 매니저 정보 수정 요청 DTO
+     * @return null
+     */
+    @PatchMapping("/{admin-id}")
+    public ResponseEntity<ApiResponse<Void>> updateManager(
+            @AuthenticationPrincipal AuthenticatedUser admin,
+            @Valid @RequestBody AdminUpdateReqDTO updateReqDTO,
+            @PathVariable("admin-id") Long adminId
+    ) {
+        if (   !UserStatus.ACTIVE.equals(admin.getStatus())
+                && !UserStatus.REJECTED.equals(admin.getStatus())
+        ) {
+            throw new AccessDeniedException(
+                    "죄송합니다. 현재 계정 상태에서는 해당 요청을 처리할 수 없습니다. (상태: " + admin.getStatus().getLabel() + ")" );
+        }
+        adminAuthService.updateAdmin(adminId, updateReqDTO);
+        return ResponseEntity.ok(new ApiResponse<>(true, "매니저 정보 수정 성공", null));
+    }
+
+    /**
+     * 관리자 삭제 API
+     * @param adminId 관리자 ID
+     * @return 성공 응답
+     */
+    @DeleteMapping("/{admin-id}")
+    public ResponseEntity<ApiResponse<Void>> deleteAdmin(
+            @AuthenticationPrincipal AuthenticatedUser admin,
+            @PathVariable("admin-id") Long adminId
+    ) {
+        if (   !UserStatus.ACTIVE.equals(admin.getStatus())
+                && !UserStatus.REJECTED.equals(admin.getStatus())
+        ) {
+            throw new AccessDeniedException(
+                    "죄송합니다. 현재 계정 상태에서는 해당 요청을 처리할 수 없습니다. (상태: " + admin.getStatus().getLabel() + ")" );
+        }
+        adminAuthService.deleteAdmin(adminId);
+        return ResponseEntity.ok(new ApiResponse<>(true, "매니저 삭제 성공", null));
+    }
 
 }
--- api/src/main/java/com/kernel/app/service/AdminAuthService.java
+++ api/src/main/java/com/kernel/app/service/AdminAuthService.java
@@ -2,8 +2,11 @@ package com.kernel.app.service;
 
 
 import com.kernel.common.admin.dto.request.AdminSignupReqDTO;
+import com.kernel.common.admin.dto.request.AdminUpdateReqDTO;
 
 public interface AdminAuthService {
 
      void join(AdminSignupReqDTO joinDTO);
+     void updateAdmin(Long adminID, AdminUpdateReqDTO updateDTO);
+     void deleteAdmin(Long adminID);
 }
