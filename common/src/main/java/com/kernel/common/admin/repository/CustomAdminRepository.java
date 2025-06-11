5ff5c63ca113c179858d6699a4dc407fb8496522
SighingOwl <whddms1208@gmail.com>
2025-06-11 11:04:04 +0900

    feat: 관리자 계정 관리 기능 개발

--- common/src/main/java/com/kernel/common/admin/repository/CustomAdminRepository.java
+++ common/src/main/java/com/kernel/common/admin/repository/CustomAdminRepository.java
@@ -2,12 +2,9 @@ package com.kernel.common.admin.repository;
 
 import com.kernel.common.admin.dto.request.AdminSearchReqDTO;
 import com.kernel.common.admin.entity.Admin;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface AdminRepository extends JpaRepository<Admin, Long>, CustomAdminRepository {
+public interface CustomAdminRepository {
+    Page<Admin> searchByConditionsWithPaging(AdminSearchReqDTO request, Pageable pageable);
 }
