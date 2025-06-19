813d05917ec1b6d1b729dc117f9565227fe973df
hyeyoooo <12qwpo50@gmail.com>
2025-06-19 16:09:44 +0900

    refactor: admin 모듈 entity, enum 추가

---
+++ admin/src/main/java/com/kernel/admin/domain/enumerate/BoardType.java
@@ -0,0 +1,16 @@
+package com.kernel.admin.domain.enumerate;
+
+public enum BoardType {
+    NOTICE("공지"),
+    EVENT("이벤트");
+
+    private String BoardName;
+
+    BoardType(String boardName) {
+        this.BoardName = boardName;
+    }
+
+    public String getBoardName() {
+        return BoardName;
+    }
+}
