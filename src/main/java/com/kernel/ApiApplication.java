80296240a5f7ac9af51694a0bebdc54058702050
SujeongJeong <suujeong0@gmail.com>
2025-06-20 00:34:09 +0900

    refactor : bootJar 설정 수정

---
+++ src/main/java/com/kernel/ApiApplication.java
@@ -0,0 +1,24 @@
+package com.kernel;
+
+import com.kernel.global.common.properties.AwsProperties;
+import com.kernel.global.common.properties.JwtProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = "com.kernel")
+@EnableJpaRepositories(basePackages = "com.kernel")
+@EntityScan(basePackages = "com.kernel")
+@EnableConfigurationProperties({
+        JwtProperties.class,
+        AwsProperties.class
+})
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
