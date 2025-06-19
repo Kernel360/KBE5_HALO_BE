e740bc72cd86651b086cd952de2c305061f136ff
SighingOwl <whddms1208@gmail.com>
2025-06-19 16:41:55 +0900

    refactor: member 모듈 manager 관련 entity, enum 추가

---
+++ member/src/main/java/com/kernel/member/domain/entity/Manager.java
@@ -0,0 +1,31 @@
+package com.kernel.member.domain.entity;
+
+import com.kernel.member.domain.enumerate.ContractStatus;
+
+import java.time.LocalDateTime;
+
+public class Manager {
+
+    // 매니저 ID
+    private Long managerId;
+
+    // 사용자 정보
+    private UserInfo userInfo;
+
+    // 특기
+    //private ServiceCategory specialty;
+
+    // 한 줄 소개
+    private String bio;
+
+    // 프로필 이미지 URL
+    private long profileImageFileId;
+
+    // 계약상태
+    private ContractStatus contractStatus;
+
+    // 계약 일시
+    private LocalDateTime contractDate;
+
+
+}
---
+++ member/src/main/java/com/kernel/member/domain/entity/ManagerStatistic.java
@@ -0,0 +1,16 @@
+package com.kernel.member.domain.entity;
+
+public class ManagerStatistic {
+
+    // 매니저 ID
+    private Long managerId;
+
+    // 예약 수
+    private Integer reservationCount;
+
+    // 리뷰 수
+    private Integer reviewCount;
+
+    // 매니저가 받은 별점 평균
+    private Double averageRating;
+}
