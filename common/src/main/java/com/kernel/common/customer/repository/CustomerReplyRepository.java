195f953acacc2ff41392bd9387e53e2212d994fc
SighingOwl <whddms1208@gmail.com>
2025-06-20 17:31:13 +0900

    remove: inquiry, reply 관련 리펙토링 이전 코드 삭제

--- common/src/main/java/com/kernel/common/customer/repository/CustomerReplyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kernel.common.customer.repository;
-
-import com.kernel.common.customer.entity.CustomerReply;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerReplyRepository extends JpaRepository<CustomerReply, Long> {
-}
