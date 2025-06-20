eb6ad8bbaa40f0d0a68d1bc1c52b087ec27799e0
SighingOwl <whddms1208@gmail.com>
2025-06-20 23:50:03 +0900

    refactor: ReplyController는 security config가 필터링 조건에 따라 admin 사용자만 접근하도록 api에 admin 추가

--- inquiry/src/main/java/com/kernel/inquiry/controller/reply/ReplyController.java
+++ inquiry/src/main/java/com/kernel/inquiry/controller/reply/ReplyController.java
@@ -7,14 +7,16 @@ import com.kernel.inquiry.service.reply.ReplyService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/reply")
+@RequestMapping("/api/admin/reply")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public class ReplyController {
 
     private final ReplyService replyService;
