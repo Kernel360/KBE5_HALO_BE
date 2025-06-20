4a7dd21989062bc06a1b4362b6f5bd97bb3708e3
SighingOwl <whddms1208@gmail.com>
2025-06-20 23:06:16 +0900

    refactor: 누락된 CustomerInquiryCategory, ManagerInquiryCategory enum 내용 추가

--- inquiry/src/main/java/com/kernel/inquiry/common/enums/ManagerInquiryCategory.java
+++ inquiry/src/main/java/com/kernel/inquiry/common/enums/ManagerInquiryCategory.java
@@ -1,4 +1,15 @@
 package com.kernel.inquiry.common.enums;
 
 public enum ManagerInquiryCategory {
+    SCHEDULE("스케줄 조정"),
+    PAYMENT("정산 / 급여"),
+    WORK_PROBLEM("업무 고충"),
+    SYSTEM("시스템 문제"),
+    ETC("기타 문의");
+
+    private String categoryName;
+
+    ManagerInquiryCategory(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
