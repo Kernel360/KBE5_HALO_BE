096f4361208c1aa1e301e7c9bb5f2c1e18384267
SighingOwl <whddms1208@gmail.com>
2025-06-20 23:53:55 +0900

    refactor: 문의사항 작성과 수정 service에 작성자 유형에 적절한 문의 카테고리를 검증하는 로직 추가, deleteInquiry는 DTO에서 inquiryId를 받는 대신 바로 Long 타입 id를 매개변수로 받아 사용하는 것으로 수정

--- inquiry/src/main/java/com/kernel/inquiry/service/inquiry/InquiryCommandServiceImpl.java
+++ inquiry/src/main/java/com/kernel/inquiry/service/inquiry/InquiryCommandServiceImpl.java
@@ -5,7 +5,6 @@ import com.kernel.inquiry.domain.entity.Inquiry;
 import com.kernel.inquiry.repository.InquiryRepository;
 import com.kernel.inquiry.service.dto.request.InquiryCreateReqDTO;
 
-import com.kernel.inquiry.service.dto.request.InquiryDeleteReqDTO;
 import com.kernel.inquiry.service.dto.request.InquiryUpdateReqDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -30,6 +29,13 @@ public class InquiryCommandServiceImpl implements InquiryCommandService {
     @Override
     public void createInquiry(InquiryCreateReqDTO request, Long authorId, AuthorType authorType) {
 
+        // 문의 카테고리 검증
+        if (authorType == AuthorType.CUSTOMER && !(request.getCategory() instanceof com.kernel.inquiry.common.enums.CustomerInquiryCategory)) {
+            throw new IllegalArgumentException("잘못된 고객 문의 카테고리입니다.");
+        } else if (authorType == AuthorType.MANAGER && !(request.getCategory() instanceof com.kernel.inquiry.common.enums.ManagerInquiryCategory)) {
+            throw new IllegalArgumentException("잘못된 관리자 문의 카테고리입니다.");
+        }
+
         Inquiry inquiry = request.toEntity(authorId, authorType);
         inquiryRepository.save(inquiry);
     }
@@ -51,19 +57,27 @@ public class InquiryCommandServiceImpl implements InquiryCommandService {
             throw new IllegalArgumentException("작성자 ID가 일치하지 않습니다.");
         }
 
+        // 문의 카테고리 검증
+        if (authorId != null && inquiry.getAuthorType() == AuthorType.CUSTOMER &&
+                !(request.getCategory() instanceof com.kernel.inquiry.common.enums.CustomerInquiryCategory)) {
+            throw new IllegalArgumentException("잘못된 고객 문의 카테고리입니다.");
+        } else if (authorId != null && inquiry.getAuthorType() == AuthorType.MANAGER &&
+                !(request.getCategory() instanceof com.kernel.inquiry.common.enums.ManagerInquiryCategory)) {
+            throw new IllegalArgumentException("잘못된 관리자 문의 카테고리입니다.");
+        }
         inquiry.update(request.toEntity());
     }
 
     /**
      * 문의사항 삭제
      *
-     * @param request   요청 DTO
+     * @param inquiryId 삭제할 문의사항 ID
      * @param authorId  작성자 ID
      */
     @Transactional
     @Override
-    public void deleteInquiry(InquiryDeleteReqDTO request, Long authorId) {
-        Inquiry inquiry = inquiryRepository.findById(request.getInquiryId())
+    public void deleteInquiry(Long inquiryId, Long authorId) {
+        Inquiry inquiry = inquiryRepository.findById(inquiryId)
                 .orElseThrow(() -> new NoSuchElementException("문의사항을 찾을 수 없습니다."));
 
         // 작성자 ID 일치
