079851617cae735e2ac6d9ee23e006d814561289
SighingOwl <whddms1208@gmail.com>
2025-06-20 23:57:47 +0900

    refactor: 클라이언트에서 LocalDate 타입으로 전달된 fromCreateAt, toCreateAt을 LocalDateTime으로 변환하는 메서드 추가

--- inquiry/src/main/java/com/kernel/inquiry/service/dto/request/InquirySearchReqDTO.java
+++ inquiry/src/main/java/com/kernel/inquiry/service/dto/request/InquirySearchReqDTO.java
@@ -3,6 +3,7 @@ package com.kernel.inquiry.service.dto.request;
 import lombok.Getter;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
@@ -10,11 +11,11 @@ public class InquirySearchReqDTO {
 
     // 작성일시 시작일
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private LocalDateTime fromCreatedAt;
+    private LocalDate fromCreatedAt;
 
     // 작성일시 종료일
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private LocalDateTime toCreatedAt;
+    private LocalDate toCreatedAt;
 
     // 답변상태
     private Boolean replyStatus;
@@ -24,5 +25,14 @@ public class InquirySearchReqDTO {
 
     // 내용키워드
     private String contentKeyword;
+
+    // LocalDate -> LocalDateTime 변환
+    public LocalDateTime getFromCreatedAt() {
+        return fromCreatedAt != null ? fromCreatedAt.atStartOfDay() : null;
+    }
+
+    public LocalDateTime getToCreatedAt() {
+        return toCreatedAt != null ? toCreatedAt.atTime(23, 59, 59) : null;
+    }
 
 }
