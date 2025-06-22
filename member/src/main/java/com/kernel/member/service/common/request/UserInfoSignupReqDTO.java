028599f4c153b2c67235760158192cba101d03dc
SujeongJeong <suujeong0@gmail.com>
2025-06-22 19:13:36 +0900

    refactor : reqDTO 위도, 경도 validation 추가

--- member/src/main/java/com/kernel/member/service/common/request/UserInfoSignupReqDTO.java
+++ member/src/main/java/com/kernel/member/service/common/request/UserInfoSignupReqDTO.java
@@ -3,8 +3,7 @@ package com.kernel.member.service.common.request;
 import com.kernel.global.domain.entity.User;
 import com.kernel.member.common.enums.Gender;
 import com.kernel.member.domain.entity.UserInfo;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -24,10 +23,16 @@ public class UserInfoSignupReqDTO {
     private Gender gender;
 
     // 위도
+    @DecimalMin(value = "-90.0", message = "주소를 다시 선택해주세요.")
+    @DecimalMax(value = "90.0", message = "주소를 다시 선택해주세요.")
+    @Digits(integer = 2, fraction = 7, message = "주소를 다시 선택해주세요.")
     @NotNull(message = "주소를 입력해주세요.")
     private BigDecimal latitude;
 
     // 경도
+    @DecimalMin(value = "-180.0", inclusive = true, message = "주소를 다시 선택해주세요.")
+    @DecimalMax(value = "180.0", inclusive = true, message = "주소를 다시 선택해주세요.")
+    @Digits(integer = 3, fraction = 7, message = "주소를 다시 선택해주세요.")
     @NotNull(message = "주소를 입력해주세요.")
     private BigDecimal longitude;
 
--- member/src/main/java/com/kernel/member/service/common/request/UserInfoUpdateReqDTO.java
+++ member/src/main/java/com/kernel/member/service/common/request/UserInfoUpdateReqDTO.java
@@ -1,7 +1,6 @@
 package com.kernel.member.service.common.request;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,10 +23,16 @@ public class UserInfoUpdateReqDTO {
     private String detailAddress;
 
     // 위도
+    @DecimalMin(value = "-90.0", message = "주소를 다시 선택해주세요.")
+    @DecimalMax(value = "90.0", message = "주소를 다시 선택해주세요.")
+    @Digits(integer = 2, fraction = 7, message = "주소를 다시 선택해주세요.")
     @NotNull(message = "주소를 입력해주세요.")
     private BigDecimal latitude;
 
     // 경도
+    @DecimalMin(value = "-180.0", inclusive = true, message = "주소를 다시 선택해주세요.")
+    @DecimalMax(value = "180.0", inclusive = true, message = "주소를 다시 선택해주세요.")
+    @Digits(integer = 3, fraction = 7, message = "주소를 다시 선택해주세요.")
     @NotNull(message = "주소를 입력해주세요.")
     private BigDecimal longitude;
 }
