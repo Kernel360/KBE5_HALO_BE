420ed612548d2de6f9dac1c336b11224f4b709d0
SighingOwl <whddms1208@gmail.com>
2025-06-23 19:27:43 +0900

    refactor: Termination 요청 테이블의 내용을 manager 상세 조회에 담아 보내므로 별도의 api와 response DTO가 필요없어 삭제 및 repository에 관련 내용 삭제

--- member/src/main/java/com/kernel/member/service/ManagerTerminationService.java
+++ member/src/main/java/com/kernel/member/service/ManagerTerminationService.java
@@ -1,11 +1,9 @@
 package com.kernel.member.service;
 
 import com.kernel.member.service.request.ManagerTerminationReqDTO;
-import com.kernel.member.service.response.ManagerTerminationRspDTO;
 
 public interface ManagerTerminationService {
 
     void requestTermination(Long managerId, ManagerTerminationReqDTO request);
-    ManagerTerminationRspDTO getTerminationDetails(Long managerId);
 
 }
