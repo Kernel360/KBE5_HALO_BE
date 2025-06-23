cbb2b2981ce17ff7bc912520f415ecb1cb990962
SighingOwl <whddms1208@gmail.com>
2025-06-23 16:48:06 +0900

    refactor: Manager 관련 Entity, Enum 추가

--- member/src/main/java/com/kernel/member/domain/entity/Manager.java
+++ member/src/main/java/com/kernel/member/domain/entity/Manager.java
@@ -1,33 +1,94 @@
 package com.kernel.member.domain.entity;
 
+import com.kernel.global.domain.entity.File;
+import com.kernel.global.domain.entity.User;
 import com.kernel.member.common.enums.ContractStatus;
 
+import com.kernel.member.service.request.ManagerUpdateInfoReqDTO;
+import com.kernel.reservation.domain.entity.ServiceCategory;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "manager")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@SuperBuilder
 public class Manager {
 
-    // 매니저 ID
-    private Long managerId;
+    @Id
+    private Long userId;
 
-    // 사용자 정보
-    private UserInfo userInfo;
+    // 매니저 ID
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "user_id")
+    private User user;
 
     // 특기
-    //private ServiceCategory specialty;
+    @ManyToOne
+    @JoinColumn
+    private ServiceCategory specialty;
 
     // 한 줄 소개
+    @Column(length = 50)
     private String bio;
 
     // 프로필 URL
-    private long profileImageFileId;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private File profileImageFileId;
 
     // 서류 파일 ID
-    // private File fileId;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private File fileId;
 
     // 계약상태
+    @Enumerated(EnumType.STRING)
+    @Column
     private ContractStatus contractStatus;
 
     // 계약 일시
+    @Column
     private LocalDateTime contractDate;
+
+    // Manager 정보 수정
+    public void update(ManagerUpdateInfoReqDTO request) {
+        if (request.getSpecialty() != null) {
+            this.specialty = request.getSpecialty();
+        }
+        if (request.getBio() != null) {
+            this.bio = request.getBio();
+        }
+    }
+
+    // 매니저 신청 승인
+    public void approve() {
+        this.contractStatus = ContractStatus.APPROVED;
+        this.contractDate = LocalDateTime.now();
+    }
+
+    // 매니저 신청 거절
+    public void reject() {
+        this.contractStatus = ContractStatus.REJECTED;
+    }
+
+    // 매니저 계약 해지 요청
+    public void requestTermination() {
+        this.contractStatus = ContractStatus.TERMINATION_PENDING;
+    }
+
+    // 매니저 계약 해지
+    public void terminate() {
+        this.contractStatus = ContractStatus.TERMINATED;
+    }
+
 }
--- member/src/main/java/com/kernel/member/domain/entity/ManagerTermination.java
+++ member/src/main/java/com/kernel/member/domain/entity/ManagerTermination.java
@@ -1,15 +1,45 @@
 package com.kernel.member.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "manager_termination")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class ManagerTermination {
+
+    @Id
+    private Long userId;
+
     // 매니저 ID
-    private Long managerId;
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "user_id")
+    private Manager manager;
 
     // 계약 해지 요청 일시
-    private String requestDate;
+    @Column(nullable = false)
+    @CreationTimestamp
+    private LocalDateTime requestAt;
 
     // 계약 해지 사유
+    @Column(nullable = false, length = 300) // 계약 해지 사유는 최대 300자
     private String reason;
 
     // 계약 해지 일시
-    private String terminationDate;
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime terminatedAt;
+
+    // 계약 해지 테이블 업데이트
+    public void updateTerminatedAt(LocalDateTime terminatedAt) {
+        this.terminatedAt = terminatedAt;
+    }
 }
