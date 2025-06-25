7b190f5cbf5709c46f53bb7ac4efedb851cce126
SighingOwl <whddms1208@gmail.com>
2025-06-25 19:10:31 +0900

    refactor: 사용자 상태 검증 로직 제거

--- reservation/src/main/java/com/kernel/reservation/controller/ManagerReservationController.java
+++ reservation/src/main/java/com/kernel/reservation/controller/ManagerReservationController.java
@@ -29,44 +29,36 @@ public class ManagerReservationController {
 
     /**
      * 매니저에게 할당된 예약 목록 조회 API (검색 조건 및 페이징 처리)
-     * @param manager 매니저
+     * @param user 매니저
      * @param searchCondDTO 검색조건DTO
      * @param pageable 페이징
      * @return 검색 조건에 따른 매니저에게 할당된 예약 목록을 응답 (페이징 포함)
      */
     @GetMapping
     public ResponseEntity<ApiResponse<Page<ManagerReservationSummaryRspDTO>>> searchManagerReservations(
-            @AuthenticationPrincipal CustomUserDetails manager,
+            @AuthenticationPrincipal CustomUserDetails user,
             @ModelAttribute ManagerReservationSearchCondDTO searchCondDTO,
             Pageable pageable
     ) {
-        if (!UserStatus.ACTIVE.equals(manager.getStatus())) {
-            throw new AccessDeniedException(
-                "죄송합니다. 현재 계정 상태에서는 해당 요청을 처리할 수 없습니다. (상태: " + manager.getStatus() + ")"
-            );
-        }
+
         Page<ManagerReservationSummaryRspDTO> responseDTOPage
-            = reservationService.searchManagerReservationsWithPaging(manager.getUserId(), searchCondDTO, pageable);
+            = reservationService.searchManagerReservationsWithPaging(user.getUserId(), searchCondDTO, pageable);
         return ResponseEntity.ok(new ApiResponse<>(true, "매니저 예약 목록 조회 성공", responseDTOPage));
     }
 
     /**
      * 매니저에게 할당된 예약 상세 조회 API
-     * @param manager 매니저
+     * @param user 매니저
      * @param reservationId
      * @return 매니저에게 할당된 예약 상세 정보를 담은 응답
      */
     @GetMapping("/{reservation-id}")
     public ResponseEntity<ApiResponse<ManagerReservationRspDTO>> getManagerReservation(
-        @AuthenticationPrincipal CustomUserDetails manager,
+        @AuthenticationPrincipal CustomUserDetails user,
         @PathVariable("reservation-id") Long reservationId
     ) {
-        if (!UserStatus.ACTIVE.equals(manager.getStatus())) {
-            throw new AccessDeniedException(
-                "죄송합니다. 현재 계정 상태에서는 해당 요청을 처리할 수 없습니다. (상태: " + manager.getStatus() + ")"
-            );
-        }
-        ManagerReservationRspDTO responseDTO = reservationService.getManagerReservation(manager.getUserId(), reservationId);
+
+        ManagerReservationRspDTO responseDTO = reservationService.getManagerReservation(user.getUserId(), reservationId);
         return ResponseEntity.ok(new ApiResponse<>(true, "매니저 예약 상세 조회 성공", responseDTO));
     }
 }
