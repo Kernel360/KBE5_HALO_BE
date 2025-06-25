f485136e8b21138056c853879d15929e8eb341dd
SighingOwl <whddms1208@gmail.com>
2025-06-25 19:11:47 +0900

    refactor: setter를 builder 패턴으로 변경

--- reservation/src/main/java/com/kernel/reservation/service/response/ManagerReservationRspDTO.java
+++ reservation/src/main/java/com/kernel/reservation/service/response/ManagerReservationRspDTO.java
@@ -3,23 +3,18 @@ package com.kernel.reservation.service.response;
 import com.kernel.reservation.domain.enums.ReservationStatus;
 
 import com.kernel.reservation.service.info.ManagerReservationDetailInfo;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
-import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
-import lombok.Setter;
-
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SuperBuilder
 public class ManagerReservationRspDTO {
 
-    /* 예약 정보 **************************************/
     // 예약ID
     private Long reservationId;
 
@@ -38,23 +33,15 @@ public class ManagerReservationRspDTO {
     // 상태
     private ReservationStatus status;
 
-    // 상태명
-    private String statusName;
-
-
-    /* 고객 정보 **************************************/
     // 고객 ID
     private Long customerId;
 
-
-    /* 서비스 상세 ************************************/
     // 추가 서비스
     private String extraServiceName;
+
     // 고객 요청사항
     private String memo;
 
-
-    /* 체크인/ 체크아웃 ********************************/
     // 체크ID
     private Long checkId;
 
@@ -72,23 +59,22 @@ public class ManagerReservationRspDTO {
 
     // ManagerReservationDetailInfo에서 필요한 필드만 포함
     public static ManagerReservationRspDTO fromInfo(ManagerReservationDetailInfo info) {
-        ManagerReservationRspDTO dto = new ManagerReservationRspDTO();
-        dto.setReservationId(info.getReservationId());
-        dto.setRequestDate(info.getRequestDate());
-        dto.setStartTime(info.getStartTime());
-        dto.setTurnaround(info.getTurnaround());
-        dto.setServiceName(info.getServiceName());
-        dto.setStatus(info.getStatus());
-        dto.setCustomerId(info.getCustomerId());
-        dto.setExtraServiceName(info.getExtraService());
-        dto.setMemo(info.getMemo());
-        dto.setCheckId(info.getReservationCheckId());
-        dto.setInTime(info.getInTime() != null ? info.getInTime().toLocalDateTime() : null);
-        dto.setInFileId(info.getInFileId());
-        dto.setOutTime(info.getOutTime() != null ? info.getOutTime().toLocalDateTime() : null);
-        dto.setOutFileId(info.getOutFileId());
-
-        return dto;
+       return  ManagerReservationRspDTO.builder()
+                .reservationId(info.getReservationId())
+                .requestDate(info.getRequestDate())
+                .startTime(info.getStartTime())
+                .turnaround(info.getTurnaround())
+                .serviceName(info.getServiceName())
+                .status(info.getStatus())
+                .customerId(info.getCustomerId())
+                .extraServiceName(info.getExtraService())
+                .memo(info.getMemo())
+                .checkId(info.getReservationCheckId())
+                .inTime(info.getInTime() != null ? info.getInTime().toLocalDateTime() : null)
+                .inFileId(info.getInFileId())
+                .outTime(info.getOutTime() != null ? info.getOutTime().toLocalDateTime() : null)
+                .outFileId(info.getOutFileId())
+                .build();
     }
 
 }
