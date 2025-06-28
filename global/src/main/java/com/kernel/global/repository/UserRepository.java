42079fbafb37672f0a9ca35a2c16e4fbe796e5ce
SujeongJeong <suujeong0@gmail.com>
2025-06-28 20:39:16 +0900

    refactor : 로그인시 계정 상태값 조건 추가

--- global/src/main/java/com/kernel/global/repository/UserRepository.java
+++ global/src/main/java/com/kernel/global/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.kernel.global.repository;
 
+import com.kernel.global.common.enums.UserRole;
+import com.kernel.global.common.enums.UserStatus;
 import com.kernel.global.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,6 +9,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    // 핸드폰 번호로 유저 찾기
-    Optional<User> findByPhone(String phone);
+    // 핸드폰 번호로 활성 유저 찾기
+    Optional<User> findByPhoneAndStatus(String phone, UserStatus status);
+
 }
