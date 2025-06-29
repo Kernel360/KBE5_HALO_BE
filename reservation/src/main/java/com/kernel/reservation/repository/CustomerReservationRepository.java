c6cb9fddaa333096819edf81a65b3a5a134fafd2
SujeongJeong <suujeong0@gmail.com>
2025-06-29 19:56:35 +0900

    refactor : 취소 가능 예약 조회 쿼리에 예약 상태 조건 추가

--- reservation/src/main/java/com/kernel/reservation/repository/CustomerReservationRepository.java
+++ reservation/src/main/java/com/kernel/reservation/repository/CustomerReservationRepository.java
@@ -1,15 +1,20 @@
 package com.kernel.reservation.repository;
 
 
-import com.kernel.sharedDomain.common.enums.ReservationStatus;
 import com.kernel.sharedDomain.domain.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface CustomerReservationRepository extends JpaRepository<Reservation, Long>, CustomCustomerReservationRepository{
 
-    // 수요자 예약 취소
-    Optional<Reservation> findByReservationIdAndUser_UserId(Long reservationId, Long userId);
+    // 수요자 취소 가능 예약 조회
+    @Query("SELECT r FROM Reservation r " +
+            "WHERE r.reservationId = :reservationId " +
+            "AND r.user.userId = :userId " +
+            "AND (r.status = 'REQUESTED' OR r.status = 'CONFIRMED')")
+    Optional<Reservation> getCancelableReservation(@Param ("reservationId")Long reservationId, @Param("userId") Long userId);
 
 }
