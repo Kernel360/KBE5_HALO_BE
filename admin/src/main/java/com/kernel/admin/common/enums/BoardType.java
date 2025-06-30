8c4c30c2f34ae8cce79706c3332606f34cb6d5ee
SighingOwl <whddms1208@gmail.com>
2025-06-30 18:02:35 +0900

    refactor: BoardType 경로 변경

--- admin/src/main/java/com/kernel/admin/common/enums/BoardType.java
+++ admin/src/main/java/com/kernel/admin/common/enums/BoardType.java
@@ -1,7 +1,13 @@
 package com.kernel.admin.common.enums;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "게시판 유형")
 public enum BoardType {
+    @Schema(description = "공지 게시판")
     NOTICE("공지"),
+
+    @Schema(description = "이벤트 게시판")
     EVENT("이벤트");
 
     private String noticeName;
