021238ba5f044ee9a90557f89e79c4f5bd4336b7
sujeong <suujeong0@gmail.com>
2025-07-04 15:59:19 +0900

    feat : 매니저 없음 예외 추가

--- reservation/src/main/java/com/kernel/reservation/common/handler/ReservationExceptionHandler.java
+++ reservation/src/main/java/com/kernel/reservation/common/handler/ReservationExceptionHandler.java
@@ -2,6 +2,7 @@ package com.kernel.reservation.common.handler;
 
 
 import com.kernel.reservation.common.exception.InsufficientPointsException;
+import com.kernel.reservation.common.exception.NoAvailableManagerException;
 import com.kernel.reservation.service.response.common.ReservationErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,6 +20,14 @@ public class ReservationExceptionHandler {
     @ExceptionHandler(InsufficientPointsException.class)
     public ResponseEntity<ReservationErrorResponse> handleAuthException(InsufficientPointsException e) {
         log.warn("InsufficientPointsException error: {}", e.getMessage());
+        ReservationErrorResponse errorResponse = new ReservationErrorResponse(e.getErrorCode());
+        return ResponseEntity.status(e.getErrorCode().getStatus()).body(errorResponse);
+    }
+
+    // 매니저 없음 예외
+    @ExceptionHandler(NoAvailableManagerException.class)
+    public ResponseEntity<ReservationErrorResponse> handleNoAvailableManagerException(NoAvailableManagerException e) {
+        log.warn("NoAvailableManagerException error: {}", e.getMessage());
         ReservationErrorResponse errorResponse = new ReservationErrorResponse(e.getErrorCode());
         return ResponseEntity.status(e.getErrorCode().getStatus()).body(errorResponse);
     }
