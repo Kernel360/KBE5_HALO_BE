42731cd14b170dbfc21eee0bdd4645b9bb3c95ae
sujeong <suujeong0@gmail.com>
2025-07-06 01:05:28 +0900

    refactor : 화면에 값 응답 가능하게 수정

--- inquiry/src/main/java/com/kernel/inquiry/common/enums/CustomerInquiryCategory.java
+++ inquiry/src/main/java/com/kernel/inquiry/common/enums/CustomerInquiryCategory.java
@@ -1,15 +1,25 @@
 package com.kernel.inquiry.common.enums;
 
-public enum CustomerInquiryCategory {
+public enum CustomerInquiryCategory implements InquiryCategory {
     RESERVATION_CHANGE("예약 및 일정 변경"),
     SERVICE_INQUIRY("서비스 내용 문의"),
     PAYMENT_ISSUE("요금 및 결제"),
     COMPLAINT("불만 및 불편사항"),
     ETC("기타 문의");
 
-    private String categoryName;
+    private String label;
 
-    CustomerInquiryCategory(String categoryName) {
-        this.categoryName = categoryName;
+    CustomerInquiryCategory(String label) {
+        this.label = label;
+    }
+
+    @Override
+    public String getLabel() {
+        return label;
+    }
+
+    @Override
+    public String toString() {
+        return label;
     }
 }
