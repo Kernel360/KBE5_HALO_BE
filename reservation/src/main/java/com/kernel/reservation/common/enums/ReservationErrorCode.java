e2247e648311f7409b3239c71e49d32e00fead68
sujeong <suujeong0@gmail.com>
2025-07-05 12:22:31 +0900

    refactor : ReservationException 사용

--- reservation/src/main/java/com/kernel/reservation/common/enums/ReservationErrorCode.java
+++ reservation/src/main/java/com/kernel/reservation/common/enums/ReservationErrorCode.java
@@ -7,11 +7,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum ReservationErrorCode {
 
-    // 포인트 부족
     INSUFFICIENT_POINTS(400,"RESERVATION-001","포인트가 부족합니다."),
-    
-    // 매니저 없음
-    NO_AVAILABLE_MANAGER(404,"RESERVATION-002","요청하신 일정에 가능한 매니저가 없습니다.");
+    NO_AVAILABLE_MANAGER(404,"RESERVATION-002","요청하신 일정에 가능한 매니저가 없습니다."),
+    NOT_FOUND_RESERVATION(404,"RESERVATION-003","예약이 존재하지 않습니다.");
 
     private final int status;
     private final String code;
