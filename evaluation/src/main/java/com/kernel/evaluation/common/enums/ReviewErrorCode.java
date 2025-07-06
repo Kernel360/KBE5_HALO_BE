f09296c7f5a6621849686a08515abb4e94ea30b3
SighingOwl <whddms1208@gmail.com>
2025-07-06 15:19:40 +0900

    refactor: 리뷰 작성 권한 없음은 403 응답 코드로 수정

--- evaluation/src/main/java/com/kernel/evaluation/common/enums/ReviewErrorCode.java
+++ evaluation/src/main/java/com/kernel/evaluation/common/enums/ReviewErrorCode.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum ReviewErrorCode {
 
-    UNAUTHORIZED(400, "REVIEW-001", "리뷰 작성 권한이 없습니다."),
+    UNAUTHORIZED(403, "REVIEW-001", "리뷰 작성 권한이 없습니다."),
     ALREADY_EXISTS_REVIEW(409, "REVIEW-002", "이미 리뷰가 작성되어 있습니다."),
     NOT_COMPLETED_RESERVATION(400, "REVIEW-003", "완료되지 않은 예약에 대해서는 리뷰를 작성할 수 없습니다.");
 
