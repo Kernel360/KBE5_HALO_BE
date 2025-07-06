9bfaf8b587efbde5be122b1cb4f03bf95c55d893
sujeong <suujeong0@gmail.com>
2025-07-07 02:16:36 +0900

    refactor : 카테고리 명 수정

--- inquiry/src/main/java/com/kernel/inquiry/common/enums/CustomerInquiryCategory.java
+++ inquiry/src/main/java/com/kernel/inquiry/common/enums/CustomerInquiryCategory.java
@@ -1,9 +1,9 @@
 package com.kernel.inquiry.common.enums;
 
 public enum CustomerInquiryCategory implements InquiryCategory {
-    RESERVATION_CHANGE("예약 및 일정 변경"),
-    SERVICE_INQUIRY("서비스 내용 문의"),
-    PAYMENT_ISSUE("요금 및 결제"),
+    RESERVATION("예약 및 일정 변경"),
+    SERVICE("서비스 내용 문의"),
+    PAYMENT("요금 및 결제"),
     COMPLAINT("불만 및 불편사항"),
     ETC("기타 문의");
 
