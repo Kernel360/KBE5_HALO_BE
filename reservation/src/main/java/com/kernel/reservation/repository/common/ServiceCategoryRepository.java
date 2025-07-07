f3937ce939d82844b15212b152b91276cb1c7687
SighingOwl <whddms1208@gmail.com>
2025-07-07 15:30:38 +0900

    refactor: 매니저 예약 상세 조회시, 추가서비스 조회 및 서비스 총 가격 계산 로직 추가

--- reservation/src/main/java/com/kernel/reservation/repository/common/ServiceCategoryRepository.java
+++ reservation/src/main/java/com/kernel/reservation/repository/common/ServiceCategoryRepository.java
@@ -9,4 +9,7 @@ public interface ServiceCategoryRepository extends JpaRepository<ServiceCategory
 
     // 서비스 카테고리 전체 조회
     List<ServiceCategory> findAllByOrderByCreatedAtAsc();
+
+    // 서비스 카테고리 이름으로 조회
+    List<ServiceCategory> findByServiceNameIn(List<String> serviceNames);
 }
