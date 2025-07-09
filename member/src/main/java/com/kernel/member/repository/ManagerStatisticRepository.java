78f131c1e084b1a0d7d3524b386e427b937f99f4
SighingOwl <whddms1208@gmail.com>
2025-07-09 14:22:54 +0900

    feat: 매니저가 체크아웃 완료 후 예약 상태가 "COMPLETED"가 되었을 때 매니저가 수행한 예약 수 +1하는 쿼리 추가

--- member/src/main/java/com/kernel/member/repository/ManagerStatisticRepository.java
+++ member/src/main/java/com/kernel/member/repository/ManagerStatisticRepository.java
@@ -2,6 +2,12 @@ package com.kernel.member.repository;
 
 import com.kernel.member.domain.entity.ManagerStatistic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ManagerStatisticRepository extends JpaRepository<ManagerStatistic, Long> {
+
+    @Modifying
+    @Query("UPDATE ManagerStatistic ms SET ms.reservationCount = ms.reservationCount + :count WHERE ms.userId = :userId")
+    void updateReservationCount(Long userId, Integer count);
 }
